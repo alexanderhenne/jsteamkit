cbb2a168ef7caa265467bd3930e9f7ae91fe6652
Lotto <prozet@live.se>
2019-02-22 21:26:54 +0100

    Update CM server list
    The previous ones are all dead.

--- src/main/java/com/jsteamkit/cm/CMServerList.java
+++ src/main/java/com/jsteamkit/cm/CMServerList.java
@@ -12,59 +12,113 @@ public class CMServerList {
 
     private static List<InitialCMServer> list = new ArrayList<>();
     static {
-        // Stockholm, Sweden
-        list.add(new InitialCMServer("185.25.180.14", 27017));
-        list.add(new InitialCMServer("185.25.180.14", 27019));
-
-        list.add(new InitialCMServer("185.25.180.15", 27017));
-        list.add(new InitialCMServer("185.25.180.15", 27018));
-        list.add(new InitialCMServer("185.25.180.15", 27019));
-
-        // Frankfurt, Germany
-        list.add(new InitialCMServer("162.254.197.41", 27017));
-        list.add(new InitialCMServer("162.254.197.41", 27018));
-        list.add(new InitialCMServer("162.254.197.41", 27019));
-        list.add(new InitialCMServer("162.254.197.41", 27020));
-        list.add(new InitialCMServer("162.254.197.41", 27021));
-
-        // London, UK
-        list.add(new InitialCMServer("162.254.196.40", 27017));
-        list.add(new InitialCMServer("162.254.196.40", 27018));
-        list.add(new InitialCMServer("162.254.196.40", 27019));
-
-        list.add(new InitialCMServer("162.254.196.41", 27017));
-        list.add(new InitialCMServer("162.254.196.41", 27018));
-        list.add(new InitialCMServer("162.254.196.41", 27019));
-
-        // Poland
-        list.add(new InitialCMServer("155.133.242.8", 27017));
-        list.add(new InitialCMServer("155.133.242.8", 27018));
-        list.add(new InitialCMServer("155.133.242.8", 27019));
-
-        // Vienna, Austria
-        list.add(new InitialCMServer("146.66.155.8", 27017));
-        list.add(new InitialCMServer("146.66.155.8", 27018));
-        list.add(new InitialCMServer("146.66.155.8", 27019));
-        list.add(new InitialCMServer("146.66.155.8", 27020));
-
-        // Chicago, USA
-        list.add(new InitialCMServer("162.254.193.44", 27018));
-        list.add(new InitialCMServer("162.254.193.44", 27019));
-        list.add(new InitialCMServer("162.254.193.44", 27020));
-
-        list.add(new InitialCMServer("162.254.193.45", 27017));
-        list.add(new InitialCMServer("162.254.193.45", 27018));
-
-        // Virginia, USA
-        list.add(new InitialCMServer("208.78.164.10", 27018));
-        list.add(new InitialCMServer("208.78.164.10", 27019));
-
-        list.add(new InitialCMServer("208.78.164.14", 27017));
-        list.add(new InitialCMServer("208.78.164.14", 27018));
+        // Sourced from https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?cellid=0
+        list.add(new InitialCMServer("162.254.198.130", 27018));
+        list.add(new InitialCMServer("162.254.198.132", 27019));
+        list.add(new InitialCMServer("162.254.198.131", 27017));
+        list.add(new InitialCMServer("162.254.198.133", 27018));
+        list.add(new InitialCMServer("162.254.198.131", 27019));
+        list.add(new InitialCMServer("162.254.198.133", 27019));
+        list.add(new InitialCMServer("162.254.198.131", 27018));
+        list.add(new InitialCMServer("162.254.198.130", 27017));
+        list.add(new InitialCMServer("162.254.198.132", 27018));
+        list.add(new InitialCMServer("162.254.198.133", 27017));
+        list.add(new InitialCMServer("162.254.198.132", 27017));
+        list.add(new InitialCMServer("162.254.198.130", 27019));
+        list.add(new InitialCMServer("162.254.197.40", 27018));
+        list.add(new InitialCMServer("162.254.196.83", 27018));
+        list.add(new InitialCMServer("162.254.196.84", 27017));
+        list.add(new InitialCMServer("162.254.196.68", 27017));
+        list.add(new InitialCMServer("155.133.248.52", 27018));
+        list.add(new InitialCMServer("162.254.196.67", 27017));
+        list.add(new InitialCMServer("155.133.248.53", 27018));
+        list.add(new InitialCMServer("155.133.248.51", 27019));
+        list.add(new InitialCMServer("162.254.196.68", 27019));
+        list.add(new InitialCMServer("162.254.196.83", 27017));
+        list.add(new InitialCMServer("162.254.197.42", 27017));
+        list.add(new InitialCMServer("155.133.248.53", 27019));
+        list.add(new InitialCMServer("155.133.248.50", 27017));
+        list.add(new InitialCMServer("162.254.196.67", 27019));
+        list.add(new InitialCMServer("162.254.196.83", 27019));
+        list.add(new InitialCMServer("155.133.248.50", 27019));
+        list.add(new InitialCMServer("155.133.248.53", 27017));
+        list.add(new InitialCMServer("155.133.248.51", 27017));
+        list.add(new InitialCMServer("155.133.248.50", 27018));
+        list.add(new InitialCMServer("162.254.196.67", 27018));
+        list.add(new InitialCMServer("162.254.197.40", 27017));
+        list.add(new InitialCMServer("162.254.196.84", 27019));
+        list.add(new InitialCMServer("162.254.197.181", 27018));
+        list.add(new InitialCMServer("162.254.197.40", 27019));
+        list.add(new InitialCMServer("155.133.248.52", 27017));
+        list.add(new InitialCMServer("155.133.248.52", 27019));
+        list.add(new InitialCMServer("162.254.196.68", 27018));
+        list.add(new InitialCMServer("155.133.248.51", 27018));
+        list.add(new InitialCMServer("162.254.197.181", 27019));
+        list.add(new InitialCMServer("162.254.197.42", 27018));
+        list.add(new InitialCMServer("162.254.197.180", 27019));
+        list.add(new InitialCMServer("162.254.196.84", 27018));
+        list.add(new InitialCMServer("162.254.197.42", 27019));
+        list.add(new InitialCMServer("162.254.197.181", 27017));
+        list.add(new InitialCMServer("162.254.197.180", 27018));
+        list.add(new InitialCMServer("162.254.197.180", 27017));
+        list.add(new InitialCMServer("146.66.152.10", 27019));
+        list.add(new InitialCMServer("146.66.152.10", 27018));
+        list.add(new InitialCMServer("146.66.152.11", 27018));
+        list.add(new InitialCMServer("146.66.152.11", 27017));
+        list.add(new InitialCMServer("146.66.152.11", 27019));
+        list.add(new InitialCMServer("146.66.152.10", 27017));
+        list.add(new InitialCMServer("185.25.182.77", 27019));
+        list.add(new InitialCMServer("185.25.182.77", 27018));
+        list.add(new InitialCMServer("185.25.182.76", 27018));
+        list.add(new InitialCMServer("185.25.182.77", 27017));
+        list.add(new InitialCMServer("185.25.182.76", 27019));
+        list.add(new InitialCMServer("185.25.182.76", 27017));
+        list.add(new InitialCMServer("162.254.192.101", 27019));
+        list.add(new InitialCMServer("162.254.192.101", 27017));
+        list.add(new InitialCMServer("162.254.192.101", 27018));
+        list.add(new InitialCMServer("162.254.192.100", 27018));
+        list.add(new InitialCMServer("162.254.192.108", 27017));
+        list.add(new InitialCMServer("162.254.192.100", 27019));
+        list.add(new InitialCMServer("162.254.192.108", 27018));
+        list.add(new InitialCMServer("162.254.192.100", 27017));
+        list.add(new InitialCMServer("162.254.192.108", 27019));
+        list.add(new InitialCMServer("162.254.192.109", 27018));
+        list.add(new InitialCMServer("155.133.246.68", 27017));
+        list.add(new InitialCMServer("162.254.192.109", 27019));
+        list.add(new InitialCMServer("162.254.192.109", 27017));
+        list.add(new InitialCMServer("155.133.246.68", 27018));
+        list.add(new InitialCMServer("155.133.246.69", 27017));
+        list.add(new InitialCMServer("155.133.246.69", 27018));
+        list.add(new InitialCMServer("146.66.155.101", 27018));
+        list.add(new InitialCMServer("146.66.155.101", 27017));
+        list.add(new InitialCMServer("155.133.246.68", 27019));
+        list.add(new InitialCMServer("146.66.155.100", 27019));
+        list.add(new InitialCMServer("146.66.155.101", 27019));
+        list.add(new InitialCMServer("155.133.246.69", 27019));
+        list.add(new InitialCMServer("146.66.155.100", 27018));
+        list.add(new InitialCMServer("146.66.155.100", 27017));
+        list.add(new InitialCMServer("162.254.193.47", 27017));
+        list.add(new InitialCMServer("162.254.193.46", 27017));
+        list.add(new InitialCMServer("162.254.193.6", 27017));
+        list.add(new InitialCMServer("162.254.193.6", 27019));
+        list.add(new InitialCMServer("162.254.193.46", 27019));
+        list.add(new InitialCMServer("162.254.193.7", 27017));
+        list.add(new InitialCMServer("162.254.193.46", 27018));
+        list.add(new InitialCMServer("162.254.193.47", 27018));
+        list.add(new InitialCMServer("162.254.193.7", 27018));
+        list.add(new InitialCMServer("162.254.193.47", 27019));
+        list.add(new InitialCMServer("162.254.193.7", 27019));
+        list.add(new InitialCMServer("162.254.193.6", 27018));
+        list.add(new InitialCMServer("155.133.230.50", 27019));
+        list.add(new InitialCMServer("155.133.230.34", 27018));
+        list.add(new InitialCMServer("155.133.230.50", 27018));
+        list.add(new InitialCMServer("155.133.230.34", 27017));
 
         // Remove servers that appear to be down
         for (Iterator<InitialCMServer> iterator = list.iterator(); iterator.hasNext();) {
-            if (iterator.next().lastPing == -1) {
+            InitialCMServer server = iterator.next();
+            if (server.lastPing == -1) {
+                System.out.println("Warning: the CM server " + server.ip + ":" + server.port + " appears to be down.");
                 iterator.remove();
             }
         }
